a866e1058daa0b7a6dfa66832a131f400e69484e
Panuganti Mugdha <144349731+PanugantiMugdha@users.noreply.github.com>
2025-07-23 21:46:06 +0530

    Create Operators.java

---
+++ Operators.java
@@ -0,0 +1,41 @@
+public class Operators 
+{
+    public static void main(String args[])
+    {
+        // //Unary operators
+        // int a=10;
+        // //int b=++a;
+        // int b=a++;
+        // System.out.println(a);
+        // System.out.println(b);
+        // int m=10;
+        // // int n=--m;
+        // int n=m--;
+        // System.out.println(m);
+        // System.out.println(n);
+        // //Relational Operators
+        // int x=10;
+        // int y=10;
+        // // System.out.println(x==y);
+        // // System.out.println(x!=y);
+        // // System.out.println(x>y);
+        // // System.out.println(x<y);
+        // // System.out.println(x>=y);
+        // // System.out.println(x<=y);
+        //Logical Operators
+        System.out.println((5<0) && (3<2));
+        System.out.println((5<0) || (3<2));
+        System.out.println(!(5<0));
+        //Assignment operators
+        int l=10;
+        // int k=l;
+        // l+=10;
+        // l-=10;
+        // l*=10;
+        // l/=10;
+        l%=10;
+        System.out.println(l);
+    }
+}
+    
+
