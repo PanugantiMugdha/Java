403bb1b0a2f2ce5eed74f2c93041d35f79925792
Panuganti Mugdha <144349731+PanugantiMugdha@users.noreply.github.com>
2025-08-02 09:40:57 +0530

    Update ArraysAsFunctionArguments.java

--- ArraysAsFunctionArguments.java
+++ ArraysAsFunctionArguments.java
@@ -1,1 +1,23 @@
-
+public class ArraysAsFunctionArguments
+{
+    public static void update(int marks[],int nonchangable)
+    {
+        nonchangable=10;
+        for(int i=0;i<marks.length;i++)
+        {
+            marks[i]=marks[i]+1;
+        }
+    }
+    public static void main(String[] args)
+    {
+        int marks[]={97,98,99};
+        int nonchangable=5;
+        update(marks, nonchangable);
+        System.out.println("nonchengable:"+nonchangable);
+        for(int i=0;i<marks.length;i++)
+        {
+            System.out.print(marks[i] + " ");
+        }
+        System.out.println();
+    }
+}
