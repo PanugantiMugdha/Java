e7dad07a3a92849ad20833f8bedb33c998e1a53a
Panuganti Mugdha <144349731+PanugantiMugdha@users.noreply.github.com>
2025-08-02 16:44:50 +0530

    Create ArraysReverse.java

---
+++ ArraysReverse.java
@@ -0,0 +1,26 @@
+public class ArraysPartA
+{
+    public static void reverseArray(int numbers[])
+    {
+        int start=0, last=numbers.length-1;
+        while(start<last)
+        {
+            int temp=numbers[last];
+            numbers[last]=numbers[start];
+            numbers[start]=temp;
+            start++;
+            last--;
+        }
+    }
+    public static void main(String[] args)
+    {
+        int numbers[]={1,2,3,4,5};
+        reverseArray(numbers);
+        for(int i=0;i<numbers.length;i++)
+        {
+            System.out.print(numbers[i]+" ");
+        }
+        System.out.println();
+
+    }
+}
