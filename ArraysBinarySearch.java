1eaa4d8b67659ed359e8c0d634c55c615c039949
Panuganti Mugdha <144349731+PanugantiMugdha@users.noreply.github.com>
2025-08-02 16:31:25 +0530

    Update ArraysBinarySearch.java

--- ArraysBinarySearch.java
+++ ArraysBinarySearch.java
@@ -1,1 +1,31 @@
-
+public class ArraysPartA
+{
+    public static int binarySearch(int numbers[],int key)
+    {
+        int mid;
+        int start=0,end=numbers.length-1;
+        while(start<=end)
+        {
+            mid=(start+end)/2;
+            if(numbers[mid]==key)
+            {
+                return mid;
+            }
+            else if(numbers[mid]<key)
+            {
+                start=mid+1;
+            }
+            else 
+            {
+                end=mid-1;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args)
+    {
+        int numbers[]={1,3,7,9,11,13};
+        int key=11;
+        System.out.println("Index is:"+binarySearch(numbers, key));
+    }
+}
