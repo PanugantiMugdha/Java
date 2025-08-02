94465c07b344f8eb0a552e8997bb566463cd5b43
Panuganti Mugdha <144349731+PanugantiMugdha@users.noreply.github.com>
2025-08-02 12:40:12 +0530

    Update ArraysLargestAndSmallest.java

--- ArraysLargestAndSmallest.java
+++ ArraysLargestAndSmallest.java
@@ -1,1 +1,36 @@
 
+
+public class ArraysLargestAndSmallest
+{
+    public static int getLargest(int numbers[])
+    {
+        int largest=Integer.MIN_VALUE;
+        for(int i=0;i<numbers.length;i++)
+        {
+            if(numbers[i]>largest)
+            {
+                largest=numbers[i];
+            }
+        }
+        return largest;
+    }
+    public static int getSmallest(int numbers[])
+    {
+        int smallest=Integer.MAX_VALUE;
+        for(int i=0;i<numbers.length;i++)
+        {
+            if(numbers[i]<smallest)
+            {
+                smallest=numbers[i];
+            }
+        }
+        return smallest;
+    }
+    public static void main(String[] args)
+    {
+        int numbers[]={1,3,5,7,11,2,4};
+        System.out.println("Largest:"+getLargest(numbers));
+        System.out.println("Smallest:"+getSmallest(numbers));
+        
+    }
+}
