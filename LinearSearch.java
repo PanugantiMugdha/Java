38d1f7f2a4d7ca2c0a30648662857209e747c17b
Panuganti Mugdha <144349731+PanugantiMugdha@users.noreply.github.com>
2025-08-02 11:32:34 +0530

    Update LinearSearch.java

--- LinearSearch.java
+++ LinearSearch.java
@@ -1,1 +1,60 @@
 
+
+public class ArraysPartA
+{
+    public static void update(int marks[],int nonchangable)
+    {
+        nonchangable=10;
+        for(int i=0;i<marks.length;i++)
+        {
+            marks[i]=marks[i]+1;
+        }
+    }
+    public static int linearSearch(int numbers[],int key)
+    {
+        for(int i=0;i<numbers.length;i++)
+        {
+            if(numbers[i]==key)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+    public static int linearSearchString(String dinner[],String menuKey)
+    {
+        for(int i=0;i<dinner.length;i++)
+        {
+            if(dinner[i]==menuKey)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args)
+    {
+        int numbers[]={2,4,6,8,10,12,14,16};
+        String dinner[]={"Chapati","Palak Panneer","Rajma","Pasta","Baingan Ka Barta"};
+        String menuKey="Rajma";
+        int key=10;
+        // int index=linearSearch(numbers, key);
+        // if(index==-1)
+        // {
+        //     System.out.println("Key Not found");
+        // }
+        // else
+        // {
+        //     System.out.println("Key found at index:"+index);
+        // }
+        int index1=linearSearchString(dinner, menuKey);
+        if(index1==-1)
+        {
+            System.out.println("Key Not found");
+        }
+        else
+        {
+            System.out.println("Key found at index:"+index1);
+        }
+    }
+}
