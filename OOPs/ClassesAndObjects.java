60d6571cc2d3d3220a9e9b8938ea3a346c2a2f8c
Panuganti Mugdha <144349731+PanugantiMugdha@users.noreply.github.com>
2025-08-03 11:56:48 +0530

    Update ClassesAndObjects.java

--- OOPs/ClassesAndObjects.java
+++ OOPs/ClassesAndObjects.java
@@ -1,1 +1,23 @@
-
+public class ClassesAndObjects
+{
+    public static void main(String[] args) {
+        Pen pen1=new Pen();
+        pen1.setColor("Pink");
+        System.out.println(pen1.color);
+        pen1.color="Purple";
+        System.out.println(pen1.color);
+    }
+}
+class Pen
+{
+    String color;
+    int tip;
+    void setColor(String newColor)
+    {
+        color=newColor;
+    }
+    void setTip(int newTip)
+    {
+        tip=newTip;
+    }
+}
