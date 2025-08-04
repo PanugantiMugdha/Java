d4ee7f1d9eb310d1f8e2cc86f5ec9932723cd217
Panuganti Mugdha <144349731+PanugantiMugdha@users.noreply.github.com>
2025-08-04 21:40:40 +0530

    Create MethodOverriding.java

---
+++ OOPs/MethodOverriding.java
@@ -0,0 +1,20 @@
+public class MethodOverriding {
+    public static void main(String[] args) {
+        Deer d1=new Deer();
+        d1.eat();
+    }
+}
+class Animal
+{
+    void eat()
+    {
+        System.out.println("Eats Anything");
+    }
+}
+class Deer extends Animal
+{
+    void eat()
+    {
+        System.out.println("Eats Grass");
+    }
+}
