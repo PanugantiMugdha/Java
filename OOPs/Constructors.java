401b45f19840902216484ab8049cf66e53203a0b
Panuganti Mugdha <144349731+PanugantiMugdha@users.noreply.github.com>
2025-08-04 10:23:45 +0530

    Create Constructors.java

---
+++ OOPs/Constructors.java
@@ -0,0 +1,14 @@
+public class Constructors {
+    public static void main(String[] args) {
+        Student s1=new Student();
+    }
+}
+class Student
+{
+    int roll;
+    String name;
+    Student()
+    {
+        System.out.println("Constructor is called..");
+    }
+}
