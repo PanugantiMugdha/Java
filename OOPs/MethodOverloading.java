f6addee8de43b4a98b946e169d03b35796c7c939
Panuganti Mugdha <144349731+PanugantiMugdha@users.noreply.github.com>
2025-08-04 21:26:44 +0530

    Create MethodOverloading.java

---
+++ OOPs/MethodOverloading.java
@@ -0,0 +1,25 @@
+public class MethodOverloading {
+    public static void main(String args[])
+    {
+        Calculator cal=new Calculator();
+        System.out.println(cal.sum(10,20));
+        System.out.println(cal.sum(3.5f,2.5f));
+        System.out.println(cal.sum(10,20,30));
+
+    }
+}
+class Calculator
+{
+    int sum(int a,int b)
+    {
+        return a+b;
+    }
+    float sum(float a,float b)
+    {
+        return a+b;
+    }
+    int sum(int a,int b,int c)
+    {
+        return a+b+c;
+    }
+}
