8345312622e2d33896f144df9b6f489ffb14d36b
Panuganti Mugdha <144349731+PanugantiMugdha@users.noreply.github.com>
2025-08-09 13:07:41 +0800

    Create PrintIncreasing.java

---
+++ RecursionPart1/PrintIncreasing.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+public class PrintIncreasing 
+{
+    public static void printInc(int n)
+    {
+        if(n==1)
+        {
+            System.out.print(n+" ");
+            return;
+        }
+        printInc(n-1);
+        System.out.print(n+" ");
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        printInc(n);
+    }
+}
