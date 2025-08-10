2f5baacb3f622d2149ef1b5004498782728e6516
Panuganti Mugdha <144349731+PanugantiMugdha@users.noreply.github.com>
2025-08-10 12:40:09 +0800

    Create CheckSortedOrNot.java

---
+++ RecursionPart1/CheckSortedOrNot.java
@@ -0,0 +1,20 @@
+public class CheckSortedOrNot
+{
+    public static boolean isSorted(int arr[],int i)
+    {
+        if(i==arr.length-1)
+        {
+            return true;
+        }
+        if(arr[i]>arr[i+1])
+        {
+            return false;
+        }
+        return isSorted(arr, i+1);
+    }
+    public static void main(String[] args) {
+        int arr[]={1,2,3,5,4};
+        // int i;
+        System.out.println(isSorted(arr, 0));
+    }
+}
