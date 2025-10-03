6e5f20ea91c82d7887b23f097c55e7aaecdeb08f
Panuganti Mugdha <144349731+PanugantiMugdha@users.noreply.github.com>
2025-10-03 20:26:37 +0530

    Create Intoduction.java

---
+++ ArrayList/Intoduction.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+public class Introduction
+{
+    public static void main(String[] args)
+    {
+        ArrayList<Integer> list=new ArrayList<>();
+        System.out.println(list);
+    }
+}
